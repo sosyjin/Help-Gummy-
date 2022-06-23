724451d083bf17ac6085a8652e24e08359bef221
sosyjin <sosyjin@gmail.com>
2022-06-24 01:03:20 +0900

    Version_0.0
    -메인 화면 UI
    -(방향키) 화면 슬라이드
    -재화 타이머, UI

---
+++ Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    float leftMove = 0;
+    float rightMove = 0;
+    float camMove = 0;
+    public float camSpeedRatio = 0;
+    public float halfMapSize = 0;
+
+    void FixedUpdate()
+    {
+        leftMove = Input.GetAxisRaw("Horizontal") * camSpeedRatio;
+        rightMove = Input.GetAxisRaw("Horizontal") * camSpeedRatio;
+        camMove = leftMove + rightMove;
+
+        if(Mathf.Abs(gameObject.transform.position.x) < halfMapSize) { // 카메라 이동 방지
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + camMove, gameObject.transform.position.y, gameObject.transform.position.z);
+        } else if (camMove * gameObject.transform.position.x < 0) {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + camMove, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
+    }
+}
---
+++ Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header ("재화")]
+    public Text goldText;
+    public Text sugarText;
+    public float maxTimer = 0;
+    int gold = 0;
+    int sugar = 0;
+    float startTimer = 0;
+    float currentTimer = 0;
+
+    [Header("배경")]
+    public GameObject [] backImages;
+    int stageLevel = 0;
+
+    private void Awake()
+    {
+        startTimer = Time.time;
+    }
+    private void Update()
+    {
+        CheckGoldTimer();
+    }
+
+    // 골드 타이머
+    void CheckGoldTimer ()
+    {
+        currentTimer = Time.time - startTimer;
+        if (currentTimer >= maxTimer) {
+            ResetGoldTimer();
+            gold += 100;
+        }
+    }
+    void ResetGoldTimer()
+    {
+        startTimer = Time.time;
+        goldText.text = gold.ToString();
+    }
+
+    // 스테이지 초기화
+    public void SetStageLevel(int inputStageLevel) {
+        // 골드 초기화
+        startTimer = Time.time;
+        gold = 0;
+        goldText.text = gold.ToString();
+
+        // 스테이지 레벨 설정
+        stageLevel = inputStageLevel;
+        for (int i = 0; i < backImages.Length; i++) // 스테이지 배경 비활성화
+            backImages[i].SetActive(false);
+        backImages[stageLevel].SetActive(true); // 선택된 스테이지 배경 활성화
+    }
+}
