4680fb2785d836e4c360c03c042aa45429f078dd
sosyjin <sosyjin@gmail.com>
2022-07-14 20:59:29 +0900

    Version_1.3
    - 난이도별 배경 적용
    - 크래프팅 UI 적용(미완)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,232 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 패럴렉스 적용을 위한 2차원 배열 선언 (*인스펙터 표시) | https://m.blog.naver.com/kijw24/221631136622 참조
+[System.Serializable]
+public class BackgroundList
+{
+    public GameObject[] backgroundImages;
+}
+
+public class GameManager : MonoBehaviour
+{
+    [Header("재화")]
+    public Text goldText;
+    public Text sugarText;
+    public float maxTimer = 0;
+    int gold = 0;
+    // int sugar = 0;
+    float startTimer = 0;
+    float currentTimer = 0;
+
+    [Header("배경")]
+    public BackgroundList[] backgroundList;
+    public GameObject mainCamera;
+    public float parallaxSpeed;
+    Camera mainCameraScript;
+    int stageLevel = 0;
+
+    [Header("오브젝트 풀")]
+    public Transform objectPool;
+
+    [Header("게임 재생, 시간")]
+    public Text gamePlayText;
+    public Text gameAccelText;
+
+    [Header("크래프팅 UI")]
+    public Animator craftingAnim;
+    public Image [] craftingImages;
+    public Text jellyNameText;
+    public Text craftingSugarText;
+    public Text craftingJuiceText;
+    int craftingCursor = 0;
+
+    private void Awake()
+    {
+        mainCameraScript = mainCamera.gameObject.GetComponent<Camera>();
+        startTimer = Time.time;
+    }
+    private void Update()
+    {
+        CheckGoldTimer();
+        Parallax();
+    }
+
+    //패럴렉스
+    void Parallax()
+    {
+        float pivotValue = -Input.GetAxisRaw("Horizontal");
+        for (int i = 0; i < backgroundList[stageLevel].backgroundImages.Length; i++) {
+            if (mainCameraScript.isCameraMoving) {
+                int parallaxSpeedRatio = int.Parse(backgroundList[stageLevel].backgroundImages[i].name.Split('_')[1]);
+                backgroundList[stageLevel].backgroundImages[i].gameObject.transform.position += new Vector3(pivotValue, 0, 0) * parallaxSpeedRatio * parallaxSpeed * Time.deltaTime;
+            }
+        }
+    }
+
+    // 스테이지 초기화
+    public void SetStageLevel(int inputStageLevel)
+    {
+        // 골드 초기화
+        startTimer = Time.time;
+        gold = 0;
+        goldText.text = gold.ToString();
+
+        // 스테이지 레벨 설정
+        stageLevel = inputStageLevel;
+        for (int i = 0; i < backgroundList.Length; i++) { // 전체 스테이지 배경 비활성화
+            for (int j = 0; j < backgroundList[i].backgroundImages.Length; j++)
+                backgroundList[i].backgroundImages[j].SetActive(false);
+        }
+        for (int i = 0; i < backgroundList[stageLevel].backgroundImages.Length; i++) // 선택된 스테이지 배경 활성화
+            backgroundList[stageLevel].backgroundImages[i].SetActive(true);
+    }
+
+    // 골드 타이머
+    void CheckGoldTimer()
+    {
+        currentTimer = Time.time - startTimer;
+        if (currentTimer >= maxTimer) {
+            ResetGoldTimer();
+            gold += 100;
+        }
+    }
+    void ResetGoldTimer()
+    {
+        startTimer = Time.time;
+        goldText.text = gold.ToString();
+    }
+
+    // 게임 가속
+    public void TimeAccelalation()
+    {
+        if (Time.timeScale == 0) {
+            return;
+        } else if (Time.timeScale < 4) {
+            Time.timeScale *= 2;
+            gameAccelText.text = "x" + Time.timeScale.ToString();
+        } else {
+            Time.timeScale = 1;
+            gameAccelText.text = "x1";
+        }
+    }
+    // 게임 정지
+    public void TimeStop()
+    {
+        if (Time.timeScale != 0) {
+            gamePlayText.text = "▶";
+            Time.timeScale = 0;
+        } else {
+            gamePlayText.text = "||";
+            Time.timeScale = int.Parse(gameAccelText.text.Replace("x", string.Empty)); // "x숫자" -> "숫자" | 가공해서 시간에 대입
+        }
+    }
+
+    // 크래프팅 젤리 이미지 전환
+    public void ChangeCraftingImage(string buttonType)
+    {
+        // 모든 이미지 비활성화
+        for (int i = 0; i < craftingImages.Length; i++)
+            craftingImages[i].gameObject.SetActive(false);
+
+        // 선택된 이미지 활성화
+        switch(buttonType) {
+            case "left":
+                if(craftingCursor == 0)
+                    craftingCursor = craftingImages.Length - 1;
+                else
+                    craftingCursor -= 1;
+                break;
+            case "right":
+                if (craftingCursor == craftingImages.Length - 1)
+                    craftingCursor = 0;
+                else
+                    craftingCursor += 1;
+                break;
+        }
+        craftingImages[craftingCursor].gameObject.SetActive(true);
+
+        // 젤리 이름 텍스트 변경
+        switch(craftingCursor) {
+            case 0:
+                jellyNameText.text = "곰 젤리";
+                break;
+            case 1:
+                jellyNameText.text = "젤리빈";
+                break;
+            case 2:
+                jellyNameText.text = "왕 꿈틀이";
+                break;
+            case 3:
+                jellyNameText.text = "버거 젤리";
+                break;
+            case 4:
+                jellyNameText.text = "젤리 푸딩";
+                break;
+        }
+    }
+
+    // 크래프팅 UI 애니메이션
+    public void PlayCraftingPanelAnimation()
+    {
+        if(craftingAnim.GetBool("panelAppear")) {
+            craftingAnim.SetBool("panelAppear", false);
+            craftingAnim.SetBool("panelDisappear", true);
+        } else {
+            craftingAnim.SetBool("panelAppear", true);
+            craftingAnim.SetBool("panelDisappear", false);
+        }
+    }
+
+    // 크래프팅 - 설탕, 과즙 수치 UI
+    public void ChangeCraftingStatus(string buttonValue)
+    {
+        string targetType = buttonValue.Split('_')[0];
+        string buttonType = buttonValue.Split('_')[1];
+        Text targetText;
+        int textValue = 0;
+
+        switch (targetType) {
+            case "sugar":
+                targetText = craftingSugarText;
+                textValue = int.Parse(targetText.text);
+                switch (buttonType)
+                {
+                    case "down":
+                        if (textValue == 0)
+                            break;
+                        else
+                            targetText.text = (textValue - 1).ToString();
+                        break;
+                    case "up":
+                        if (textValue == 10)
+                            break;
+                        else
+                            targetText.text = (textValue + 1).ToString();
+                        break;
+                }
+                break;
+            case "juice":
+                targetText = craftingJuiceText;
+                textValue = int.Parse(targetText.text);
+                switch (buttonType)
+                {
+                    case "down":
+                        if (textValue == 0)
+                            break;
+                        else
+                            targetText.text = (textValue - 1).ToString();
+                        break;
+                    case "up":
+                        if (textValue == 10)
+                            break;
+                        else
+                            targetText.text = (textValue + 1).ToString();
+                        break;
+                }
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    [Header("유닛 프리팹")]
+    public GameObject bearJellyPrefab;
+    public GameObject normalJellyPrefab;
+    public GameObject longJellyPrefab;
+    public GameObject archJellyPrefab;
+    public GameObject burgerJellyPrefab;
+    [Space]
+    public GameObject aPrefab;
+    public GameObject bPrefab;
+    public GameObject cPrefab;
+    public GameObject dPrefab;
+    public GameObject ePrefab;
+
+    [Header("오브젝트 풀")]
+    public GameObject objectPool;
+    [Range(30, 100)]
+    public int poolSize;
+
+    GameObject [] bearJellyPool;
+    GameObject [] normalJellyPool;
+    GameObject [] longJellyPool;
+    GameObject [] archJellyPool;
+    GameObject [] burgerJellyPool;
+
+    GameObject [] aPool;
+    GameObject [] bPool;
+    GameObject [] cPool;
+    GameObject [] dPool;
+    GameObject [] ePool;
+
+    private void Awake()
+    {
+        // 아군 유닛 풀
+        bearJellyPool = new GameObject[poolSize];
+        normalJellyPool = new GameObject[poolSize];
+        longJellyPool = new GameObject[poolSize];
+        archJellyPool = new GameObject[poolSize];
+        burgerJellyPool = new GameObject[poolSize];
+
+        // 적군 유닛 풀
+        aPool = new GameObject[poolSize];
+        bPool = new GameObject[poolSize];
+        cPool = new GameObject[poolSize];
+        dPool = new GameObject[poolSize];
+        ePool = new GameObject[poolSize];
+
+        GeneratePool();
+    }
+
+
+    // 오브젝트 풀링
+    void GeneratePool()
+    {
+        for(int i = 0; i < poolSize; i++) {
+            bearJellyPool[i] = Instantiate(bearJellyPrefab, new Vector2(-6.5f, bearJellyPrefab.transform.position.y), UnityEngine.Quaternion.identity);
+            bearJellyPool[i].SetActive(false);
+            bearJellyPool[i].transform.parent = objectPool.transform;
+
+            normalJellyPool[i] = Instantiate(normalJellyPrefab, new Vector2(-6.5f, normalJellyPrefab.transform.position.y), UnityEngine.Quaternion.identity);
+            normalJellyPool[i].SetActive(false);
+            normalJellyPool[i].transform.parent = objectPool.transform;
+
+            longJellyPool[i] = Instantiate(longJellyPrefab, new Vector2(-6.5f, longJellyPrefab.transform.position.y), UnityEngine.Quaternion.identity);
+            longJellyPool[i].SetActive(false);
+            longJellyPool[i].transform.parent = objectPool.transform;
+
+            archJellyPool[i] = Instantiate(archJellyPrefab, new Vector2(-6.5f, archJellyPrefab.transform.position.y), UnityEngine.Quaternion.identity);
+            archJellyPool[i].SetActive(false);
+            archJellyPool[i].transform.parent = objectPool.transform;
+
+            burgerJellyPool[i] = Instantiate(burgerJellyPrefab, new Vector2(-6.5f, burgerJellyPrefab.transform.position.y), UnityEngine.Quaternion.identity);
+            burgerJellyPool[i].SetActive(false);
+            burgerJellyPool[i].transform.parent = objectPool.transform;
+
+            // ================================================================================================================ //
+
+            aPool[i] = Instantiate(aPrefab, new Vector2(6.5f, aPrefab.transform.position.y), UnityEngine.Quaternion.identity);
+            aPool[i].SetActive(false);
+            aPool[i].transform.parent = objectPool.transform;
+
+            bPool[i] = Instantiate(bPrefab, new Vector2(6.5f, bPrefab.transform.position.y), UnityEngine.Quaternion.identity);
+            bPool[i].SetActive(false);
+            bPool[i].transform.parent = objectPool.transform;
+
+            cPool[i] = Instantiate(cPrefab, new Vector2(6.5f, cPrefab.transform.position.y), UnityEngine.Quaternion.identity);
+            cPool[i].SetActive(false);
+            cPool[i].transform.parent = objectPool.transform;
+
+            dPool[i] = Instantiate(dPrefab, new Vector2(6.5f, dPrefab.transform.position.y), UnityEngine.Quaternion.identity);
+            dPool[i].SetActive(false);
+            dPool[i].transform.parent = objectPool.transform;
+
+            ePool[i] = Instantiate(ePrefab, new Vector2(6.5f, ePrefab.transform.position.y), UnityEngine.Quaternion.identity);
+            ePool[i].SetActive(false);
+            ePool[i].transform.parent = objectPool.transform;
+        }
+    }
+    GameObject [] targetPool;
+    public void GenerateUnitObject(string objectName)
+    {
+        if (Time.timeScale == 0)
+            return;
+        switch(objectName) {
+            case "bearJelly":
+                targetPool = bearJellyPool;
+                break;
+            case "normalJelly":
+                targetPool = normalJellyPool;
+                break;
+            case "longJelly":
+                targetPool = longJellyPool;
+                break;
+            case "archJelly":
+                targetPool = archJellyPool;
+                break;
+            case "burgerJelly":
+                targetPool = burgerJellyPool;
+                break;
+
+            case "a":
+                targetPool = aPool;
+                break;
+            case "b":
+                targetPool = bPool;
+                break;
+            case "c":
+                targetPool = cPool;
+                break;
+            case "d":
+                targetPool = dPool;
+                break;
+            case "e":
+                targetPool = ePool;
+                break;
+        }
+        for (int i = 0; i < targetPool.Length; i++) {
+            if (!targetPool[i].activeSelf) {
+                targetPool[i].SetActive(true);
+                return;
+            }
+        }
+        return;
+    }
+}
