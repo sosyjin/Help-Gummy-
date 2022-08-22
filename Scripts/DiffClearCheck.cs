e0725268df909fb17d9e310a59d495ac784ffcd4
sosyjin <sosyjin@gmail.com>
2022-08-22 13:37:50 +0900

    Version_2.6
    - 주석 가독성 개선
    - UI 배치 수정

--- Scripts/DiffClearCheck.cs
+++ Scripts/DiffClearCheck.cs
@@ -6,34 +6,31 @@ using UnityEngine.UI;
 // 스테이지 클리어 여부를 확인하고 클리어 뱃지를 가시화
 public class DiffClearCheck : MonoBehaviour 
 {
-    
     //public Image[] clearBadges;
-
     public GameObject LockPanel_Normal;
     public GameObject LockPanel_Hard;
 
     private void Awake()
     {
-
-        // 배지 비가시화 (에러 방지)
-        /*
+        /* 배지 비가시화 (에러 방지)
         clearBadges[0].enabled = false;
         clearBadges[1].enabled = false;
         clearBadges[2].enabled = false;
         */
 
+        // 스테이지 잠금화면 가시화 (에러 방지)
         LockPanel_Normal.SetActive(true);
         LockPanel_Hard.SetActive(true);
     }
 
     void OnEnable()
     {
-        // 클리어 여부 확인
+        // 스테이지 클리어 여부 확인
         bool isEasyCleared = PlayerPrefs.GetInt("isEasyCleared") == 1;
         bool isNormalCleared = PlayerPrefs.GetInt("isNormalCleared") == 1;
         bool isHardCleared = PlayerPrefs.GetInt("isHardCleared") == 1;
 
-        // 배지 가시화
+        // UI 적용
         if (isEasyCleared) {
             //clearBadges[0].enabled = true;
             LockPanel_Normal.SetActive(false);
