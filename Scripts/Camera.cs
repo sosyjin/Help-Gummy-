9733d04bf71bb8a71fefb2ca6d2af530a69483cd
sosyjin <sosyjin@gmail.com>
2022-09-29 20:44:03 +0900

    Version_3.7
    - 스크롤링 방식을 키보드 입력에서 화면 터치로 변환

--- Scripts/Camera.cs
+++ Scripts/Camera.cs
@@ -6,18 +6,23 @@ public class Camera : MonoBehaviour
 {
     public float camSpeed = 0;
     public float halfMapSize = 0;
-    float horizontalMove = 0;
     [System.NonSerialized]
+    public float horizontalMove = 0;
     public bool isCameraMoving = false;
 
     void Update()
     {
         // 카메라 이동
-        horizontalMove = Input.GetAxisRaw("Horizontal") * camSpeed;
-        isCameraMoving = false;
         if( (Mathf.Abs(gameObject.transform.position.x) < halfMapSize) || (horizontalMove * gameObject.transform.position.x < 0) ) { // 카메라 범위 외 이동 방지
             isCameraMoving = true;
             gameObject.transform.position += new Vector3(horizontalMove, 0, 0) * Time.deltaTime;
+        } else {
+            isCameraMoving = false;
         }
+    }
+
+    public void CamMove(int moveDirection)
+    {
+        horizontalMove = moveDirection * camSpeed;
     }
 }
